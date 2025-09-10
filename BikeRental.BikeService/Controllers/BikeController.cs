5dc84e082ba96f634dc01afd075692626aa956f7
tirey93 <tirey93@gmail.com>
2025-09-10 09:31:00 +0200

    added application layer

--- BikeRental.BikeService/Controllers/BikeController.cs
+++ BikeRental.BikeService/Controllers/BikeController.cs
@@ -1,3 +1,5 @@
+using BikeRental.BikeService.Controllers.Commands;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BikeRental.BikeService.Controllers
@@ -6,28 +8,27 @@ namespace BikeRental.BikeService.Controllers
     [Route("[controller]")]
     public class BikeController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<BikeController> _logger;
+        private readonly IMediator _mediator;
 
-        public BikeController(ILogger<BikeController> logger)
+        public BikeController(ILogger<BikeController> logger, IMediator mediator)
         {
             _logger = logger;
+            _mediator = mediator;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpPost]
+        public async Task<ActionResult> Create([FromBody] CreateBikeCommand command)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            try
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                await _mediator.Send(command);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { ex.Message });
+            }
         }
     }
 }
---
+++ BikeRental.BikeService/Controllers/Commands/CreateBikeCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace BikeRental.BikeService.Controllers.Commands
+{
+    public class CreateBikeCommand : IRequest
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public DateOnly LastServiceDate { get; set; }
+    }
+
+    public class CreateBikeCommandHandler : IRequestHandler<CreateBikeCommand>
+    {
+        public Task Handle(CreateBikeCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
