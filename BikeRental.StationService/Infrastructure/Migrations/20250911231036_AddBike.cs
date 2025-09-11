b0799d9fdb44610244c70325bda76673474ee819
tirey93 <tirey93@gmail.com>
2025-09-12 01:13:41 +0200

    added migration

---
+++ BikeRental.StationService/Infrastructure/Migrations/20250911231036_AddBike.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BikeRental.StationService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBike : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExternalBikes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ExternalId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExternalBikes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExternalBikes");
+        }
+    }
+}
